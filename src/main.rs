a79f3ff485e5a5366cee4f33e32b46471cec91ac
Huy Duong <duonghuy1401@gmail.com>
2022-12-04 14:41:18 +0700

    respond ping command

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 // Uncomment this block to pass the first stage
-// use std::net::TcpListener;
-
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
 
                 let mut buf = [0; 512];
